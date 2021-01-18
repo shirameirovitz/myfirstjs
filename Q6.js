8c90413ed4a8e72d92b330ba9978278091980db9
shirameirovitz <shirameirovi@gmail.com>
2021-01-18 17:48:15 +0200

    answer Q6

--- Q6.js
+++ Q6.js
@@ -1,8 +1,22 @@
-for (let inputList= Number[x, y, z]) {
-let n=inputList.length;
-inputList= (x+y+z)/n;
-x=80;
-y=100;
-z=90;
-alert(inputList);
+let grades= [80, 70, 100];
+let total = 0;
+for(let i = 0; i < grades.length; i++) {
+    total += grades[i];
+}
+var avg = total / grades.length;
+
+if (avg<60) {
+    console.log("F");
+}
+else if (avg<70) {
+    console.log("D");
+}
+else if (avg<80) {
+    console.log("C");
+}
+else if (avg<90) {
+    console.log("B");
+}
+else if (avg<100) {
+    console.log("A");
 }
