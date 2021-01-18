1f5755185c5169778689643543a1bed3ef32c806
shirameirovitz <shirameirovi@gmail.com>
2021-01-18 20:20:43 +0200

    answer for Q9

---
+++ Q9.js
@@ -0,0 +1,18 @@
+//x__
+for (let uno =0; uno<10; uno++){
+     //_x_
+for (let dos =0; dos<10; dos++){
+    //__x
+    for (let tres =1; tres<10; tres++){
+        let i=0;
+        let j=0;
+ i > 99; i = uno + 10 * dos + 100 * tres; i<1000;
+ j > 99; j = uno*uno*uno + dos*dos*dos + tres*tres*tres; i<1000;
+
+if (i===j) {
+    console.log(j);
+}
+}    
+}
+}
+ 
