1ccf6e0bbfc1ec9995457de91f8ca9765eb6e2de
shirameirovitz <shirameirovi@gmail.com>
2021-01-18 13:32:49 +0200

    answer for Q1

---
+++ Q1.js
@@ -0,0 +1,9 @@
+let number1= Number(prompt("insert first number"));
+let number2= Number(prompt("insert second number"));
+
+if (number1 > number2) {
+    alert (number1);
+} 
+else {
+    alert (number2);
+}
