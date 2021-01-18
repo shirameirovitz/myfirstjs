81574daab416298871cd001bb0275612440c9526
shirameirovitz <shirameirovi@gmail.com>
2021-01-18 17:19:35 +0200

    answer Q5

---
+++ Q5.js
@@ -0,0 +1,3 @@
+for (let i=0; i<=15; i++) {
+    console.log (i%2===0?i+" even":i+" odd");
+} 
---
+++ Q6.js
@@ -0,0 +1,8 @@
+for (let inputList= Number[x, y, z]) {
+let n=inputList.length;
+inputList= (x+y+z)/n;
+x=80;
+y=100;
+z=90;
+alert(inputList);
+}
