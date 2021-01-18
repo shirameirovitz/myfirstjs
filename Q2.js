bdcf405d22eab55bfb2b076c607f5fc842c06ecb
shirameirovitz <shirameirovi@gmail.com>
2021-01-18 14:16:15 +0200

    answer for Q2

---
+++ Q2.js
@@ -0,0 +1,28 @@
+let x=3;
+let y=-7;
+let z= 2;
+
+if (x>0 && y>0 && z>0){
+    alert ("sign +");
+}
+else if(x<0 && y>0 && z>0){
+    alert ("sign -");
+}
+else if(x>0 && y<0 && z>0){
+    alert ("sign -");
+}
+else if(x>0 && y>0 && z<0){
+    alert ("sign -");
+}
+else if(x<0 && y<0 && z>0){
+    alert ("sign +");
+}
+else if(x>0 && y<0 && z<0){
+    alert ("sign +");
+}
+else if(x<0 && y<0 && z<0){
+    alert ("sign -");
+}
+else if(x<0 && y>0 && z<0){
+    alert ("sign +");
+}
